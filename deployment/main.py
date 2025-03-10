e45f208242fc68c96cc39a20d918d32d38bb570d
Julien.Hansen <julien.hansen@student.uliege.be>
2025-03-10 08:36:02 +0100

    :boom: Basic API Full Gcloud integration

--- deployment/main.py
+++ deployment/main.py
@@ -52,6 +52,4 @@ def submit():
 
 
 if __name__ == '__main__':
-    print(f"Starting Server")
-    serve(app, host=HOST, port=PORT, threads=2, connection_limit=100)
-    #app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, port=5100)
