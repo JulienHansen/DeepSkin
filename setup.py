bf64a33b6ff4e891f74456475ac09fedeb93234a
Julien.Hansen <julien.hansen@student.uliege.be>
2025-03-12 14:28:57 +0100

    :construction: Starting Vertex IA implementation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    "torch",
+    "torchvision",
+    "numpy",
+    "pandas",
+    "Pillow",
+    "scikit-learn",
+    "wandb",
+    "tqdm",
+    "datasets",
+    "cloudml-hypertune",
+    "kaggle",
+    "google-cloud-secret-manager"
+]
+
+setup(
+    name='deepskin_vertex',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='DeepSkin vertex integration',
+)
---
+++ vertex/main_vertex.py
@@ -0,0 +1,60 @@
+from google.cloud import aiplatform
+from datetime import datetime
+
+# --- CONFIG ---
+PRE_BUILT_TRAINING_CONTAINER_IMAGE_URI = "europe-docker.pkg.dev/vertex-ai/training/pytorch-gpu.1-13.py310:latest"
+
+APP_NAME = "deep_skin"
+PROJECT_ID = "deepskin-451908" # Project ID 
+BUCKET_URI = "gs://deepskin_bucket" #
+LOCATION = "europe-west1"
+
+PYTHON_PACKAGE_APPLICATION_DIR = "."
+source_package_file_name = f"{PYTHON_PACKAGE_APPLICATION_DIR}/dist/deepskin_trainer-0.1.tar.gz"
+python_package_gcs_uri = f"{BUCKET_URI}/train/deepskin_trainer-0.1.tar.gz"
+python_module_name = "models.train" 
+
+# Initialize Vertex SDK
+aiplatform.init(project=PROJECT_ID, staging_bucket=BUCKET_URI)
+
+# Define variables
+TIMESTAMP = datetime.now().strftime("%Y%m%d%H%M%S")
+JOB_NAME = f"{APP_NAME}-training-{TIMESTAMP}"
+
+print(f"Starting training job: {JOB_NAME}")
+print(f"Using container: {PRE_BUILT_TRAINING_CONTAINER_IMAGE_URI}")
+print(f"Python package GCS URI: {python_package_gcs_uri}")
+
+# Create a Custom Training Job
+job = aiplatform.CustomPythonPackageTrainingJob(
+    display_name=JOB_NAME,
+    python_package_gcs_uri=python_package_gcs_uri,
+    python_module_name=python_module_name,
+    container_uri=PRE_BUILT_TRAINING_CONTAINER_IMAGE_URI,
+    location=LOCATION
+)
+
+# Define training arguments
+training_args = [
+    "--data_path", "gs://deepskin_bucket/archive",
+    "--epochs", "10",
+    "--batch_size", "64",
+    "--lr", "0.0003",
+    "--train_split", "0.8",
+    "--device", "cuda",
+    "--img_size", "224",
+    "--optimizer", "AdamW",
+    "--save_path", "gs://trained_deepskin_model/trained_models/",
+    "--vertex_ai"
+]
+
+# Submit the Job to Vertex AI
+job.run(
+    replica_count=1,
+    machine_type="n1-standard-8",
+    accelerator_type="NVIDIA_TESLA_T4",
+    accelerator_count=1,
+    args=training_args,
+    sync=True,
+    service_account="808565425437-compute@developer.gserviceaccount.com"
+)
