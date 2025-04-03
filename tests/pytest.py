4883ac952f1863aff2c60e445bdc46ecbfad466b
Clément <letouriste19@yahoo.fr>
2025-04-03 13:54:38 +0200

    add pytest

---
+++ tests/pytest.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+import requests
+import os
+
+BACKEND_URL = os.getenv("BACKEND_URL")
+
+def test_backend_route():
+    assert BACKEND_URL is not None, 
+    print(BACKEND_URL)
+    response = requests.get(f"{BACKEND_URL}/") 
+    assert response.status_code == 200
