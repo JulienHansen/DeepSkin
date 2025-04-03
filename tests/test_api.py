6a277d40a4d77bbfd6174c77063e377fecdecab6
Clément <letouriste19@yahoo.fr>
2025-04-03 15:08:28 +0200

    pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,7 +5,7 @@
 import requests
 import os
 
-BACKEND_URL = os.getenv("BACKEND_URL")
+BACKEND_URL = os.getenv("BACKEND_URL", "https://deepskin-back-ster3eab3q-ew.a.run.app")
 
 def test_backend_route():
     assert BACKEND_URL is not None, "Backend URL is not defined"
