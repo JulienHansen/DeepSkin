21c3a6e75110fe3594ba4bff4647e5eff6a8eca2
Clément <letouriste19@yahoo.fr>
2025-04-03 14:04:07 +0200

    change deploy.yml

--- tests/pytest.py
+++ tests/pytest.py
@@ -8,7 +8,7 @@ import os
 BACKEND_URL = os.getenv("BACKEND_URL")
 
 def test_backend_route():
-    assert BACKEND_URL is not None, 
+    assert BACKEND_URL is not None
     print(BACKEND_URL)
     response = requests.get(f"{BACKEND_URL}/") 
     assert response.status_code == 200
